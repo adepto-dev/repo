74fb6c6ef89ec2429edf21b164090bf04febc611
adepto-dev <testststststs236@gmail.com>
2025-06-29 20:46:37 -0300

    Update jetsmart_monitor.py

--- jetsmart_monitor.py
+++ jetsmart_monitor.py
@@ -29,7 +29,7 @@ class JetSmartScraper:
 
     def setup_driver(self):
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")  # Headless moderno
+        chrome_options.add_argument("--headless")  # Headless moderno
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
